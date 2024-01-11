58fa4b860237cd3c831eee90eb14f88bcc4eb9bc
Karim Mk <mk47blast@protonmail.com>
2024-01-11 14:37:17 +0100

    Pushing this for now. maybe I'll style it later

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
 window.onload = startApp();
 
+function startApp() {
+  const location = getLocation();
+
+  if (location === "") {
+    return renderApp("jijel");
+  }
+
+  renderApp(location)
+}
+
+function getLocation() {
+  return encodeURIComponent(document.querySelector(".search-input").value);
+}
+
+async function renderApp(location) {
+  try {
+    let data = await fetchWeather(location);
+
+    const bruh = new Date()
+    console.log(bruh.toLocaleDateString())
+    const something = data["forecast"];
+    console.log(data)
+    console.log(something)
+
+    const forecast = data["forecast"]["forecastday"];
+    const current = data["current"];
+    const locationName = data["location"];
+
+    console.log(forecast)
+
+    currentDay(current, locationName);
+    day(forecast);
+    astro(forecast);
+  } catch (err) {
+    alert(e)
+  }
+}
+
 async function fetchWeather(location) {
   const request = new Request(
     `http://api.weatherapi.com/v1/forecast.json?key=0156e23da18f4b8d8ac205307240901&q=${location}&days=3`,
@@ -32,7 +70,7 @@ function day(forecast) {
   for (let i = 0; i < forecast.length; i++) {
     date[i].textContent = `${forecast[i]["date"]}`;
     condition[i].textContent = `Condition: ${forecast[i]["day"]["condition"]["text"]}`;
-    maxtemp[i].textContent = `min tempreture: ${forecast[i]["day"]["mintemp_c"]}`;
+    maxtemp[i].textContent = `Min tempreture: ${forecast[i]["day"]["mintemp_c"]}`;
     mintemp[i].textContent = `Max tempreture: ${forecast[i]["day"]["maxtemp_c"]}`;
     rainchance[i].textContent = `Chance of rain: ${forecast[i]["day"]["daily_chance_of_rain"]}`;
   }
@@ -49,35 +87,8 @@ function currentDay(current, locationName) {
   timeAndDate.textContent = `Today: ${current["last_updated"]}`;
   currentCondition.textContent = `Condition: ${current["condition"]["text"]}`
   temp.textContent = `Tempreture: ${current["temp_c"]}`;
-  wind.textContent = `Wind: ${current["wind_kph"]}kph`
+  wind.textContent = `Wind: ${current["wind_kph"]} kph`
 }
 
 const btn = document.querySelector(".search-btn");
 btn.addEventListener("click", startApp);
-
-async function startApp() {
-  try {
-    const location = encodeURIComponent(document.querySelector(".search-input").value);
-
-    let data = await fetchWeather(location);
-
-    const bruh = new Date()
-    console.log(bruh.toLocaleDateString())
-    const something = data["forecast"];
-    console.log(data)
-    console.log(something)
-
-    const forecast = data["forecast"]["forecastday"];
-    const current = data["current"];
-    const locationName = data["location"];
-
-    console.log(forecast)
-
-    currentDay(current, locationName);
-    day(forecast);
-    astro(forecast);
-  } catch(err) {
-    alert(e)
-  }
-}
-
